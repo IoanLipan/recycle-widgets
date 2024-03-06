6a4a3692517478299d39a64a90dfee71b706428c
Lipan Ioan <lipanioan420@gmail.com>
2024-03-06 09:21:26 +0200

    add font, some progress on elements

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,39 @@
 <template>
-  <RecycleWidget
-    :id="1"
-    type="plastic bottles"
-    :amount="100"
-    action="collects"
-    selectedColor="blue"
-    active
-    linked
-  />
-  <RecycleWidget
-    :id="2"
-    type="trees"
-    :amount="10"
-    action="plants"
-    selectedColor="green"
-    active
-    linked
-  />
-  <RecycleWidget
-    :id="3"
-    type="carbon"
-    :amount="100"
-    action="offsets"
-    selectedColor="beige"
-    active
-    linked
-  />
+  <div class="w-screen h-screen flex font-sans items-center justify-center">
+    <div class="flex flex-col p-10 rounded-xl shadow-2xl gap-2">
+      <h1 class="text-3xl">Per products widgets</h1>
+      <hr class="w-full bg-grey-800 h-4" />
+      <div class="flex">
+        <RecycleWidget
+          :id="1"
+          type="plastic bottles"
+          :amount="100"
+          action="collects"
+          selectedColor="blue"
+          active
+          linked
+        />
+        <RecycleWidget
+          :id="2"
+          type="trees"
+          :amount="10"
+          action="plants"
+          selectedColor="green"
+          active
+          linked
+        />
+        <RecycleWidget
+          :id="3"
+          type="carbon"
+          :amount="100"
+          action="offsets"
+          selectedColor="beige"
+          active
+          linked
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
--- src/components/RecycleWidget.vue
+++ src/components/RecycleWidget.vue
@@ -1,8 +1,30 @@
 <template>
-  <article>
-    <header class="flex w-24 bg-blue-500">
+  <article class="flex flex-col items-center w-56">
+    <header
+      :class="translateColor(selectedColor)"
+      class="flex items-center h-16 p-4 w-full gap-2 rounded-lg bg-blue-500"
+    >
       <img src="@/assets/greensparkLogo.svg" />
+      <div>
+        <span class="text-sm">This product {{ action }}</span>
+        <br />
+        <span>{{ amount }} {{ type }}</span>
+      </div>
     </header>
+    <div class="w-full">
+      <div class="flex justify-between items-center">
+        <p>Badge colour</p>
+        <div class="flex gap-1 items-center">
+          <span
+            v-for="color in colorsAvailable"
+            :key="color"
+            class="flex h-4 w-4 border-gray-100 border-[1px] hover:border-2"
+            :class="translateColor(color)"
+            @click="selectColor(color)"
+          />
+        </div>
+      </div>
+    </div>
   </article>
 </template>
 
@@ -36,9 +58,31 @@ export default defineComponent({
       type: Boolean,
       default: false,
     },
-    selectedColor: {
-      type: String,
-      default: "green",
+  },
+  data() {
+    return {
+      colorsAvailable: ["white", "black", "blue", "green", "beige"],
+      selectedColor: "blue",
+    };
+  },
+  computed: {},
+  methods: {
+    translateColor(color: string | undefined) {
+      switch (color) {
+        case "white":
+          return "bg-white";
+        case "black":
+          return "bg-black";
+        case "blue":
+          return "bg-blue-500";
+        case "green":
+          return "bg-green-700";
+        case "beige":
+          return "bg-amber-100 text-green-800";
+      }
+    },
+    selectColor(color: string) {
+      this.selectedColor = color;
     },
   },
 });
